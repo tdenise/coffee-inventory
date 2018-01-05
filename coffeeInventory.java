440a26f3467335ce4b776b70561c3d0c43669035
rpdemilt <35045836+rpdemilt@users.noreply.github.com>
2018-01-04 21:43:45 -0800

    Add files via upload

---
+++ coffeeInventory.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+public class coffeeInventory{
+    public static void main(String[] args){
+        Scanner reader = new Scanner(new File("shipments.txt"));
+        ArrayList<Integer> incoming = new ArrayList<Integer>();
+        ArrayList<Integer> outgoing = new ArrayList<Integer>();
+        int n;
+        while(reader.hasNext()){
+            n = reader.nextInt();
+            if(n < 0){
+                outgoing.add(n);
+            } else {
+                incoming.add(n);
+            }
+        }
+        System.out.println("Incoming: ");
+        for(int i = 0; i < incoming.size(); i++){
+            System.out.println(incoming.get(i));
+        }
+        System.out.println("Outgoing: ");
+        for(int i = 0; i < outgoing.size(); i++){
+            System.out.println(outgoing.get(i));
+        }
+        int incomingTotal = 0, outgoingTotal = 0, total;
+        for(int i = 0; i < incoming.size();i++){
+            incomingTotal += incoming.get(i);
+        }
+        for(int i = 0; i < outgoing.size();i++){
+            outgoingTotal += outgoing.get(i);
+        }   
+        total = incomingTotal + outgoingTotal;
+        System.out.println("Total: " + total);
+        if(total < 0){
+            System.out.println("Today was a stock loss");
+        }else{
+        System.out.println("Today was a stock gain");
+        }
+     }
+}
